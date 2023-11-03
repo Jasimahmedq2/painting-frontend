3063621370d4b83afc15b992877d0fd6cfbf1e0f
jasimahmedq2 <jasimahmed90852417@gmail.com>
2023-11-03 19:00:11 +0600

    added filtering pagination and sorting for manage service page

--- src/app/(dashboardLayout)/dashboard/admin/manage_service/page.tsx
+++ src/app/(dashboardLayout)/dashboard/admin/manage_service/page.tsx
@@ -1,129 +1,171 @@
 "use client";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/UI/alert-dialog";
-import {
-  useGetAllServiceQuery,
-  useRemoveServiceMutation,
-} from "@/redux/service/serviceApiSlice";
+
+import Loading from "@/app/loading";
+import ManageServiceCard from "@/components/UI/manageServiceCard";
+import { useDebounced } from "@/redux/hooks";
+import { useGetAllServiceQuery } from "@/redux/service/serviceApiSlice";
 import { authKey } from "@/utilites/authkey";
 import { getFromLocalStorage } from "@/utilites/local-storage";
-import { StarFilled } from "@ant-design/icons";
-import { message } from "antd";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect } from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import {
+  Col,
+  InputNumber,
+  Row,
+  Slider,
+  Space,
+  Pagination,
+  Select,
+  Input,
+} from "antd";
+import type { PaginationProps } from "antd";
+import { SetStateAction, useState } from "react";
+
+const sortByOptions = [
+  {
+    label: "sortBy",
+    options: [
+      { label: "price", value: "price" },
+      { label: "name", value: "name" },
+      { label: "description", value: "description" },
+    ],
+  },
+];
+const sortOrderOptions = [
+  {
+    label: "sortOrder",
+    options: [
+      { label: "asc", value: "asc" },
+      { label: "desc", value: "desc" },
+    ],
+  },
+];
 
 const ManageService = () => {
   const token = getFromLocalStorage(authKey);
-  const { data, isLoading: SisLoading } = useGetAllServiceQuery(token);
-  const [removeService, { isLoading, isSuccess, isError }] =
-    useRemoveServiceMutation();
 
-  useEffect(() => {
-    if (isSuccess) {
-      message.success("successfully deleted a service");
-    }
-    if (isLoading) {
-      message.loading("loading");
-    }
-    if (isError) {
-      message.error("something went wrong");
-    }
-  }, [isLoading, isError, isSuccess]);
+  const query: Record<string, any> = {};
+  const [minPrice, setMinPrice] = useState<number>(50);
+  const [sortOrder, setSortOrder] = useState<string>("asc");
+  const [sortBy, setSortBy] = useState<string>("price");
+  const [search, setSearch] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
 
-  const handleDeleteService = async (id: string) => {
-    const removeInfo = {
-      id,
-      token,
-    };
-    await removeService(removeInfo);
+  query["minPrice"] = minPrice;
+  query["sortOrder"] = sortOrder;
+  query["page"] = page;
+  query["perPage"] = size;
+  query["sortField"] = sortBy;
+  const debouncedSearchTerm = useDebounced({
+    searchQuery: search,
+    delay: 600,
+  });
+
+  if (!!debouncedSearchTerm) {
+    query["search"] = debouncedSearchTerm;
+  }
+
+  const handleChangeSortBy = (value: string) => {
+    setSortBy(value);
   };
+
+  const handleChangeSortOrder = (value: string) => {
+    setSortOrder(value);
+  };
+
+  const handlePriceSlider = (newValue: number) => {
+    setMinPrice(newValue);
+  };
+
+  const onShowSizeChange: PaginationProps["onShowSizeChange"] = (
+    current: number,
+    pageSize: number
+  ) => {
+    console.log(current, pageSize);
+    setPage(current);
+    setSize(pageSize);
+  };
+
+  const queryInfo = {
+    token,
+    param: { ...query },
+  };
+
+  const { data, isLoading: SisLoading } = useGetAllServiceQuery(queryInfo);
+  const totalResults = data?.data?.totalResults;
+
+  const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
+    setSearch(e.target.value);
+  };
+
+  if (SisLoading) {
+    return <Loading />;
+  }
 
   return (
     <div className=" sm:min-h-screen py-8 sm:py-12">
       <div>
-        <h2 className=" p-12 text-4xl font-bold sm:text-5xl">Services</h2>
+        <h2 className=" p-12 text-2xl font-bold sm:text-5xl">Services</h2>
+      </div>
+      <div>
+        <Row className="sm:pl-12 pl-2">
+          <Col span={12}>
+            <Slider
+              min={50}
+              max={10000}
+              onChange={handlePriceSlider}
+              value={typeof minPrice === "number" ? minPrice : 0}
+            />
+          </Col>
+          <Col span={4}>
+            <InputNumber
+              min={50}
+              max={10000}
+              style={{ margin: "0 16px" }}
+              value={minPrice}
+              onChange={handlePriceSlider}
+            />
+          </Col>
+        </Row>
+        <div className="flex justify-between items-center sm:pl-12 sm:pr-12 pt-4 pl-2 pr-2  space-x-2">
+          <Input
+            onChange={handleSearch}
+            style={{
+              width: "400px",
+            }}
+            size="large"
+            placeholder="search services"
+            prefix={<SearchOutlined />}
+          />
+          <div className="sm:flex sm:space-x-2 sm:space-y-0 space-y-2">
+            <Select
+              defaultValue="price"
+              style={{ width: 200 }}
+              onChange={handleChangeSortBy}
+              options={sortByOptions}
+            />
+            <Select
+              defaultValue="asc"
+              style={{ width: 200 }}
+              onChange={handleChangeSortOrder}
+              options={sortOrderOptions}
+            />
+          </div>
+        </div>
       </div>
       <div className="p-12 grid cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data?.data?.map((service: any) => {
-          return (
-            <div
-              key={service?._id}
-              className="max-w-sm rounded overflow-hidden shadow-lg"
-            >
-              <Image
-                width={400}
-                height={200}
-                src={service?.image}
-                alt="Sunset in the mountains"
-              />
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{service?.name}</div>
-                <p className="text-gray-700 text-base">
-                  {service.description.length > 30
-                    ? service.description.substring(0, 30) + "..."
-                    : service.description}
-                </p>
-              </div>
-              <div className=" px-6 flex justify-between items-center">
-                <h2 className="text-xl font-bold text-gray-800">
-                  ${service?.price}
-                </h2>
-                <p className="font-md">
-                  4 <StarFilled />
-                </p>
-              </div>
-              <div className="px-6 flex items-center justify-between pt-4 pb-2">
-                <Link href={`/dashboard/admin/manage_service/${service?._id}`}>
-                  <button className="bg-gradient-to-r from-blue-500 to-indigo-600 hover:from-blue-600 hover:to-indigo-700 text-white font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
-                    edit
-                  </button>
-                </Link>
-
-                <AlertDialog>
-                  <AlertDialogTrigger className="bg-red-400 border text-white font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer uppercase">
-                    {" "}
-                    delete
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      <AlertDialogTitle>
-                        Are you absolutely sure? to delete the service?
-                      </AlertDialogTitle>
-                      <AlertDialogDescription>
-                        serviceName: {service?.name}
-                      </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                      <AlertDialogCancel>Cancel</AlertDialogCancel>
-                      <AlertDialogAction
-                        onClick={() => handleDeleteService(service?._id)}
-                      >
-                        Delete
-                      </AlertDialogAction>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
-
-                {/* <button
-                  onClick={() => handleRemove(service?._id)}
-                  className="bg-red-400 border text-white font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
-                >
-                  delete
-                </button> */}
-              </div>
-            </div>
-          );
+        {data?.data?.results?.map((service: any) => {
+          return <ManageServiceCard key={service?._id} service={service} />;
         })}
+      </div>
+      <div className="w-1/2 mx-auto pt-8">
+        <Pagination
+          showSizeChanger
+          onChange={onShowSizeChange}
+          defaultCurrent={1}
+          defaultPageSize={10}
+          total={totalResults}
+        />
       </div>
     </div>
   );
---
+++ src/components/UI/manageServiceCard.tsx
@@ -0,0 +1,101 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/UI/alert-dialog";
+import { useRemoveServiceMutation } from "@/redux/service/serviceApiSlice";
+import { message } from "antd";
+import { useEffect } from "react";
+import { getFromLocalStorage } from "@/utilites/local-storage";
+import { authKey } from "@/utilites/authkey";
+
+const ManageServiceCard = ({ service }: { service: any }) => {
+  const token = getFromLocalStorage(authKey);
+  const [removeService, { isLoading, isSuccess, isError }] =
+    useRemoveServiceMutation();
+  useEffect(() => {
+    if (isSuccess) {
+      message.success("successfully deleted a service");
+    }
+    if (isLoading) {
+      message.loading("loading");
+    }
+    if (isError) {
+      message.error("something went wrong");
+    }
+  }, [isLoading, isError, isSuccess]);
+
+  const handleDeleteService = async (id: string) => {
+    const removeInfo = {
+      id,
+      token,
+    };
+    await removeService(removeInfo);
+  };
+  return (
+    <div
+      key={service?._id}
+      className="max-w-sm rounded overflow-hidden shadow-lg"
+    >
+      <Image
+        width={400}
+        height={200}
+        src={service?.image}
+        alt="Sunset in the mountains"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{service?.name}</div>
+        <p className="text-gray-700 text-base">
+          {service.description.length > 30
+            ? service.description.substring(0, 30) + "..."
+            : service.description}
+        </p>
+      </div>
+      <div className=" px-6 flex justify-between items-center">
+        <h2 className="text-xl font-bold text-gray-800">${service?.price}</h2>
+      </div>
+      <div className="px-6 flex items-center justify-between pt-4 pb-2">
+        <Link href={`/dashboard/admin/manage_service/${service?._id}`}>
+          <button className="bg-gradient-to-r from-blue-500 to-indigo-600 hover:from-blue-600 hover:to-indigo-700 text-white font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+            edit
+          </button>
+        </Link>
+
+        <AlertDialog>
+          <AlertDialogTrigger className="bg-red-400 border text-white font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer uppercase">
+            {" "}
+            delete
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you absolutely sure? to delete the service?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                serviceName: {service?.name}
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => handleDeleteService(service?._id)}
+              >
+                Delete
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
+    </div>
+  );
+};
+
+export default ManageServiceCard;
